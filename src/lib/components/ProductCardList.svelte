4c488046b9cb870b29351a1ef40dc3a52db3665f
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-02-12 11:55:05 +0000

    Added blurred background to the sides of the list

--- src/lib/components/ProductCardList.svelte
+++ src/lib/components/ProductCardList.svelte
@@ -1,19 +1,54 @@
 <script lang="ts">
-	import ProductCard from "./ProductCard.svelte";
+    import ProductCard from "./ProductCard.svelte";
+    import { fade } from 'svelte/transition';
 
-	let { products = [] } = $props();
+    let { products = [] } = $props();
+
+    let scrollContainer: HTMLDivElement;
+    let showRightFade = $state(true);
+    let showLeftFade = $state(false);
+
+    function handleScroll() {
+        if (!scrollContainer) return;
+
+        const { scrollLeft, scrollWidth, clientWidth } = scrollContainer;
+        const isAtEnd = scrollLeft + clientWidth >= scrollWidth - 10; // 10px thresh
+        showRightFade = !isAtEnd;
+
+        showLeftFade = scrollLeft > 10;
+    }
 </script>
 
-<div class="overflow-x-auto scrollbar-hide py-4">
-	<div class="flex flex-row gap-6">
-		{#each products as product}
-			<div class="shrink-0">
-				<ProductCard
-					image={product.image}
-					brand={product.brand}
-					name={product.name}
-				/>
-			</div>
-		{/each}
-	</div>
+<div class="relative">
+    <div
+        class="overflow-x-auto scrollbar-hide py-4"
+        bind:this={scrollContainer}
+        onscroll={handleScroll}
+    >
+        <div class="flex flex-row gap-6">
+            {#each products as product}
+                <div class="shrink-0">
+                    <ProductCard
+                        image={product.image}
+                        brand={product.brand}
+                        name={product.name}
+                    />
+                </div>
+            {/each}
+        </div>
+    </div>
+
+    {#if showLeftFade}
+        <div
+            transition:fade={{ duration: 300 }}
+            class="absolute left-0 top-0 bottom-0 w-24 bg-linear-to-r from-bg-primary via-bg-primary/30 to-transparent pointer-events-none"
+        ></div>
+    {/if}
+
+    {#if showRightFade}
+        <div
+            transition:fade={{ duration: 300 }}
+            class="absolute right-0 top-0 bottom-0 w-24 bg-linear-to-l from-bg-primary via-bg-primary/30 to-transparent pointer-events-none"
+        ></div>
+    {/if}
 </div>
